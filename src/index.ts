449d497d182941a2cf1cda472908a66e1bfb9297
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-03-13 19:43:50 -0300

    retired conection test in index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
-import { db } from './database/knex'
+
 import dotenv from 'dotenv'
 import { userRouter } from './routers/userRouter'
 import { postRouter } from './routers/postRouter'
@@ -15,24 +15,6 @@ app.listen(Number(process.env.PORT), () => {
     console.log(`Servidor rodando na porta ${Number(process.env.PORT)}`)
 })
 
-app.get("/ping", async (req: Request, res: Response) => {
-    try {
-				const result = await db("users")
-        res.status(200).send({ message: "Pong!", result })
-    } catch (error) {
-        console.log(error)
-
-        if (req.statusCode === 200) {
-            res.status(500)
-        }
-
-        if (error instanceof Error) {
-            res.send(error.message)
-        } else {
-            res.send("Erro inesperado")
-        }
-    }
-})
 
 app.use("/users",userRouter)
 app.use("/posts",postRouter)
